061898d08c4de3620007cfbe318ebde80c19707d
nelsonrg <rnelsonxy@gmail.com>
2020-12-02 09:38:47 -0500

    Day 2 solutions

---
+++ day2/day2_part1.py
@@ -0,0 +1,19 @@
+#!/home/bobby/anaconda3/bin/python3
+
+def read_file(filename):
+    with open(filename) as file:
+        return [line.rstrip('\n') for line in file]
+
+def check_passwords(pwd_list):
+    counter = 0
+    for line in pwd_list:
+        rule, password = line.split(':')
+        ranges, letter = rule.split()
+        min, max = [int(x) for x in ranges.split('-')]
+        if (password.count(letter) >= min) and (password.count(letter) <= max):
+            counter = counter + 1
+    return counter
+
+if __name__ == "__main__":
+    input = read_file("input.txt")
+    print(check_passwords(input))
