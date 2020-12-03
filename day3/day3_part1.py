c7f45de80a235a494d45c1825d96345966c37629
nelsonrg <rnelsonxy@gmail.com>
2020-12-03 10:32:42 -0500

    Day 3 solutions

---
+++ day3/day3_part1.py
@@ -0,0 +1,24 @@
+#!/home/bobby/anaconda3/bin/python3
+
+def read_file(filename):
+    with open(filename) as file:
+        return [list(line.rstrip('\n')) for line in file]
+
+def calculate_trees(coordinates):
+    h_slope = 3 # move right 3 times
+    v_slope = 1 # move down 1 time
+    width = len(coordinates[0])
+    h_pos = 0
+    v_pos = 0
+    tree_counter = 0
+    while (v_pos < len(coordinates)):
+        if coordinates[v_pos][h_pos % width] == '#':
+            tree_counter += 1
+        h_pos += h_slope
+        v_pos += v_slope
+    return tree_counter
+    
+
+if __name__ == "__main__":
+    input = read_file("input.txt")
+    print(calculate_trees(input))
